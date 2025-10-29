077813888be7273bf3d90407e3761f4e54ca6ca1
Parmesh B <101285245+Parmesh119@users.noreply.github.com>
2025-10-30 00:15:01 +0530

    Remove server listen log from index.js
    Remove server listening log statement.

--- server/index.js
+++ server/index.js
@@ -36,7 +36,4 @@ app.get("/api/getkey", (req, res) => {
   res.status(200).json({ key: process.env.RAZORPAY_API_KEY });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 module.exports = app;
