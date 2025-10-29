1f6287aad059eaf118cec569ade584709f239283
Parmesh B <101285245+Parmesh119@users.noreply.github.com>
2025-10-29 23:30:15 +0530

    Replace local signup image with logo URL
    Removed local signup image and replaced with logo URL.

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -111,11 +111,6 @@ export default function SignUpThree() {
       <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
         <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
           <div className="mb-2 flex justify-center">
-            <img
-              src={signupImg}
-              width={100}
-              alt="Furniture Rentals"
-            />
 
             <img src="https://i.ibb.co/hHDy6pv/logo.png" width={100} alt="Furniture Rentals" />
           </div>
